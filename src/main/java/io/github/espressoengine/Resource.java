f036c8744b1c510eeb8f56b147675a3d913cb206
PastThePixels <pastthescreen@gmail.com>
2022-12-13 19:05:39 -0700

    Add a constructor to Resource.java

--- src/main/java/io/github/espressoengine/Resource.java
+++ src/main/java/io/github/espressoengine/Resource.java
@@ -3,6 +3,9 @@ package io.github.espressoengine.sound;
 import java.io.File;
 
 class Resource extends File {
-  // Now I can add any extra utility functions should I need to in the future. Adding this class right now ensures
-  // that when that does happen users don't have to rewrite all their programs to replace File instances with this.
+    // Now I can add any extra utility functions should I need to in the future. Adding this class right now ensures
+    // that when that does happen users don't have to rewrite all their programs to replace File instances with this.
+    public Resource(String path) {
+        super(path);
+    }
 }
